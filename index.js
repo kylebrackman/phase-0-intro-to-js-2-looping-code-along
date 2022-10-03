2fc9725e60c3fcdcca9b9d4448b3f4f14baff60f
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-02 20:44:20 -0600

    commit tree b99a14e3b551bececbc813a0a9b992b7b324cc0e

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    const thankYous = []
+    for (let i = 0; i < name.length; i++) {
+        thankYous.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous
+}
+ 
+
+function countDown(i) {
+    while (i >=0) {
+        console.log(i)
+        i--
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(name, phrase) {
+//     const newArray = []
+//     for (let i = 0; i < name.length; i++) {
+//         newArray.push (`Thank you, ${name[i]}, for the wonderful ${phrase} gift!`)
+//     }
+//     return newArray 
+// }
+
+// function countDown(x) {
+//     while (x >= 0) {
+//         console.log(x)
+//     x--
+//     }
+// }
